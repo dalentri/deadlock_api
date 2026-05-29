d5f73d3be12c385908422176bbb006c98744b035
dalentri <aydenjmason05@gmail.com>
2026-05-29 11:31:27 -0500

    Initialized scraper.ts with bun fetch to url

---
+++ src/scraper.ts
@@ -0,0 +1,25 @@
+export {};
+
+const heroes = ["Abrams", "Apollo", "Bebop"];
+let current_hero: string = "Abrams";
+
+const params = new URLSearchParams({
+  action: "parse",
+  format: "json",
+  text: `{{infobox hero| key = ${current_hero}}}`,
+  title: "Heroes",
+});
+
+// Turn it into a percent encoded string
+params.toString();
+
+// Append it to the end of the API request
+const url = `https://deadlock.wiki/api.php?${params}`;
+
+const response = await fetch(url);
+const html = await response.text();
+
+const final = JSON.parse(html);
+let final_text = final.parse.text["*"];
+
+console.log(final_text);
