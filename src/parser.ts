5627741ac054d3a926131550735402313e643679
dalentri <aydenjmason05@gmail.com>
2026-05-30 11:05:56 -0500

    Renamed variable to camelcase for proper convention

--- src/parser.ts
+++ src/parser.ts
@@ -2,9 +2,9 @@ import { readdir } from "node:fs/promises";
 import * as cheerio from "cheerio";
 
 const path_prefix: string = "./data/characters/";
-const heroes_json_files: string[] = await readdir(path_prefix);
+const heroesJsonFiles: string[] = await readdir(path_prefix);
 
-for (const hero_file of heroes_json_files) {
+for (const hero_file of heroesJsonFiles) {
   const html: string = await Bun.file(path_prefix + hero_file).text();
   const $ = cheerio.load(html);
 }
