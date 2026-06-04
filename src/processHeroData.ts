0cb21a6486c99c9c0d9089eae80d8a510821ac3c
dalentri <aydenjmason05@gmail.com>
2026-06-03 19:58:40 -0500

    Added a top-level scoped call in processHeroData.tsto run on custom package.json command

--- src/processHeroData.ts
+++ src/processHeroData.ts
@@ -1,25 +1,31 @@
 import * as cheerio from "cheerio";
 import { parseHeroData } from "./parser";
 import { heroBuilder } from "./builder/heroBuilder";
+import { readdir } from "node:fs/promises";
 
 // take html files that were parsed from deadlock wiki and parse them into json
 export async function processHeroData(
   pathPrefix: string,
   heroesHtmlFiles: string[],
 ): Promise<any> {
-  let allHeroData: any[] = [];
-
   for (const heroFile of heroesHtmlFiles) {
     let heroData: any[] = [];
 
     const html: string = await Bun.file(pathPrefix + heroFile).text();
     const $ = cheerio.load(html);
 
+    // Create an array of heroes with their full values
     const rawData: any[] = parseHeroData($, heroData, heroFile);
 
+    // Put the hero values into the Hero interface to
     const heroJson = JSON.stringify(heroBuilder(rawData), null, 2);
     const heroJsonName = heroFile.replace(".html", ".json");
     Bun.write(`./data/charactersJson/` + heroJsonName, heroJson);
   }
-  return allHeroData;
 }
+
+// Runs on script call =====
+const characterHtmlPathPrefix: string = "../data/charactersHtml/";
+const heroHtmlFiles = await readdir(characterHtmlPathPrefix);
+
+processHeroData(characterHtmlPathPrefix, heroHtmlFiles);
