7542bc886ee7e3f3d41d9d2d9eac3e2cb86350f5
dalentri <aydenjmason05@gmail.com>
2026-06-03 19:35:14 -0500

    Removed function from processHeroData for future package.json script

--- src/processHeroData.ts
+++ src/processHeroData.ts
@@ -1,25 +1,22 @@
 import * as cheerio from "cheerio";
 import { parseHeroData } from "./parser";
 import { heroBuilder } from "./builder/heroBuilder";
+import { readdir } from "node:fs/promises";
 
-// take html files that were parsed from deadlock wiki and parse them into json
-export async function processHeroData(
-  pathPrefix: string,
-  heroesHtmlFiles: string[],
-): Promise<any> {
-  let allHeroData: any[] = [];
+let allHeroData: any[] = [];
 
-  for (const heroFile of heroesHtmlFiles) {
-    let heroData: any[] = [];
+const pathPrefix = "../data/charactersHtml/";
+const heroesHtmlFiles = await readdir(pathPrefix);
 
-    const html: string = await Bun.file(pathPrefix + heroFile).text();
-    const $ = cheerio.load(html);
+for (const heroFile of heroesHtmlFiles) {
+  let heroData: any[] = [];
 
-    const rawData: any[] = parseHeroData($, heroData, heroFile);
+  const html: string = await Bun.file(pathPrefix + heroFile).text();
+  const $ = cheerio.load(html);
 
-    const heroJson = JSON.stringify(heroBuilder(rawData), null, 2);
-    const heroJsonName = heroFile.replace(".html", ".json");
-    Bun.write(`./data/charactersJson/` + heroJsonName, heroJson);
-  }
-  return allHeroData;
+  const rawData: any[] = parseHeroData($, heroData, heroFile);
+
+  const heroJson = JSON.stringify(heroBuilder(rawData), null, 2);
+  const heroJsonName = heroFile.replace(".html", ".json");
+  Bun.write(`./data/charactersJson/` + heroJsonName, heroJson);
 }
