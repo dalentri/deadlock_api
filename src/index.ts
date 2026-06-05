e02179926872cfc83090eac25bfcacc3a35fe591
dalentri <aydenjmason05@gmail.com>
2026-06-04 22:05:12 -0500

    Added cache function to index.js for quick API responses and added server initialization function with error handling and ensured initial execution of initCache function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-import { parseHeroData } from "./parser";
-import { heroBuilder } from "./builder/heroBuilder";
-import { readdir } from "node:fs/promises";
-import * as cheerio from "cheerio";
 import express from "express";
+import { readFile } from "node:fs/promises";
+import { readdir } from "node:fs/promises";
+import { Hero } from "./types/hero";
+import { start } from "node:repl";
 
-const app = express();
-const port = 3000;
+const heroSummaryCache: any[] = [];
+const heroFullDataIndex: Record<string, Hero> = {};
+
+async function initCache() {
+  const files = await readdir("../data/charactersJson/");
+
+  for (const heroJsonFile in files) {
+    const filePath = `../data/charactersJson/${heroJsonFile}`;
+    const rawData = await readFile(filePath, "utf-8");
+
+    const characterData = JSON.parse(rawData);
+
+    const slug: string = characterData.name
+      .strip()
+      .toLowerCase()
+      .replaceAll(" ", "-");
+
+    const { name, damagePerSecond, health } = characterData;
+
+    heroSummaryCache.push({ slug, name, damagePerSecond, health });
+    heroFullDataIndex[slug] = characterData;
+  }
+}
 
 // Start API at port 3000
-app.listen(port, () => {
-  console.log("Server initialized.");
-});
+async function startServer() {
+  try {
+    const app = express();
+    const port = 3000;
+
+    await initCache();
+    console.log("Cache implemented successfully.");
+
+    app.listen(port, () => {
+      console.log("Server initialized at port 3000.");
+    });
+  } catch (error) {
+    console.log("Server failed to start:", error);
+  }
+}
+
+startServer();
