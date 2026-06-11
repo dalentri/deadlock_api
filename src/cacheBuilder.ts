e81320571b3fff9e6d5de5c63558f617095e5e96
dalentri <dalentri@noreply.codeberg.org>
2026-06-11 13:18:09 -0500

    Replaced herocache file with localized summary cache and removed initCache function in index

---
+++ src/cacheBuilder.ts
@@ -0,0 +1,8 @@
+import { HeroModel } from "./models/Hero";
+
+let heroSummaryCache: any[] = [];
+
+export async function initCache(): Promise<any> {
+  const heroes = await HeroModel.find({}, { slug: 1, name: 1 });
+  heroSummaryCache = heroes;
+}
