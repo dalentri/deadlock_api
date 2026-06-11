37db9717fef7763dff76d8b3c5d6120734725820
dalentri <dalentri@noreply.codeberg.org>
2026-06-11 13:18:09 -0500

    Added lean to heroModel query to return JSON objects instead of Mongoose Documents

--- src/cacheBuilder.ts
+++ src/cacheBuilder.ts
@@ -3,7 +3,7 @@ import { HeroModel } from "./models/Hero";
 let heroSummaryCache: any[] = [];
 
 export async function initCache(): Promise<any> {
-  const heroes = await HeroModel.find({}, { slug: 1, name: 1 });
+  const heroes = await HeroModel.find({}, { slug: 1, name: 1 }).lean();
   heroSummaryCache = heroes;
 }
 
